45a5210619d584a7197967e4c00f46ad2b03be1d
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2022-02-17 23:04:56 -0300

    ëxercicio do curso

---
+++ bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,29 @@
+package bytebank;
+
+public class CriaConta {
+	public static void main(String[] args) {
+			
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem: "+primeiraConta.saldo);
+		System.out.println("segunda conta tem: "+segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("sao a mesma conta");
+		}else {
+			System.out.println("nao sao a mesma conta");
+		}
+	}
+}
---
+++ bytebank/src/bytebank/testaMetodo.java
@@ -0,0 +1,26 @@
+package bytebank;
+
+public class testaMetodo {
+	
+	public static void main(String[] args) {
+		
+		Conta contaDoLucas = new Conta();
+		contaDoLucas.saldo = 100;
+		contaDoLucas.deposita(50);
+		System.out.println(contaDoLucas.saldo);
+		contaDoLucas.saca(20);
+		System.out.println(contaDoLucas.saldo);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		if(contaDaMarcela.transfere(300, contaDoLucas)) {
+			System.out.println("transferencia com sucesso");
+		}else {
+			System.out.println("faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoLucas.saldo);
+	}
+	
+	
+}
