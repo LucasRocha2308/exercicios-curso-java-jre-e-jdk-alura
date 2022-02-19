111bc1c2d786a22a25f28a127737f1e75deaf028
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2022-02-19 11:49:10 -0300

    ëxercicio bytebank static

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -3,12 +3,15 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total;
 	
 	
 	public Conta(int agencia, int numero) {
+		Conta.total++;
+		//System.out.println("o total de contas eh "+Conta.total);
 		this.agencia = agencia;
 		this.numero = numero;
-		System.out.println("estou criando uma conta "+ this.numero);
+		//System.out.println("estou criando uma conta "+ this.numero);
 	}
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -67,6 +70,12 @@ public class Conta {
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
+	
+	public static int getTotal() {
+		return 	Conta.total;
+	}
+	
+	
 	
 	
 }
